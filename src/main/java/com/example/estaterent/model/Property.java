a1715be7d87b43945f3098e08672f53551ac5e44
David <david_ristov@live.com>
2020-04-20 19:08:22 +0200

    added setter,getter

--- src/main/java/com/example/estaterent/model/Property.java
+++ src/main/java/com/example/estaterent/model/Property.java
@@ -18,4 +18,11 @@ public class Property {
     @NonNull
     private String name;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
--- src/main/java/com/example/estaterent/model/Record.java
+++ src/main/java/com/example/estaterent/model/Record.java
@@ -30,4 +30,67 @@ public class Record {
     @ManyToOne
     private Property property;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Instant getAvailableFromDate() {
+        return availableFromDate;
+    }
+
+    public void setAvailableFromDate(Instant availableFromDate) {
+        this.availableFromDate = availableFromDate;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Property getProperty() {
+        return property;
+    }
+
+    public void setProperty(Property property) {
+        this.property = property;
+    }
 }
